b2d05d33310d702c27e89dbc35a882b80388e779
JoshuaMage <jdelrosario2121@gmail.com>
2025-01-16 14:49:31 +0800

    55% anime figures

--- src/routes/components/productsFigures/+page.svelte
+++ src/routes/components/productsFigures/+page.svelte
@@ -18,6 +18,12 @@
 
 	//hovering product description
 	let descriptionHovering = false;
+	//showing selected item
+	let selectedItem = null;
+
+	function changeImage(index) {
+		currentIndexes[selectedItem.name] = index;
+	}
 
 	if (typeof window !== 'undefined') {
 		const storedIndexes = localStorage.getItem('currentIndexes');
@@ -150,11 +156,7 @@
 	}
 
 	$: sortedData = getSortedData(data, sortOption);
-	$: imageSizeClass = gridCols === 3 ? 'md:grid-rows-[30rem]' : 'md:grid-rows-[25rem]'; 
-
-	
-	
-	
+	$: imageSizeClass = gridCols === 3 ? 'md:grid-rows-[30rem]' : 'md:grid-rows-[25rem]';
 </script>
 
 <div class="mx-auto sm:flex sm:flex-col md:grid md:grid-rows-[90%_10%]">
@@ -163,8 +165,10 @@
 		style="grid-template-columns: repeat({gridCols}, minmax(0, 1fr)); grid-template-rows: repeat({gridRows}, minmax(0, 1fr));"
 	>
 		{#each displayedData as [name, item]}
-			<div class={`grid rounded-2xl bg-black md:mb-5 md:mr-8 md:grid-rows-[30rem], ${imageSizeClass}`}>
-				<div class="relative m-auto mt-4 h-full w-full pb-3">
+			<div
+				class={`md:grid-rows-[30rem], grid rounded-2xl bg-black md:mb-5 md:mr-8 ${imageSizeClass}`}
+			>
+				<div class="group relative m-auto mt-4 h-full w-full pb-3">
 					<img
 						src={item.image[currentIndexes[name]]}
 						alt={`${name} image ${currentIndexes[name] + 1}`}
@@ -173,19 +177,24 @@
 						onmouseenter={() => (descriptionHovering = true)}
 						onmouseleave={() => (descriptionHovering = false)}
 					/>
+					<button
+						class="absolute bottom-14 left-1/2 -translate-x-1/2 transform rounded-full bg-[#3926df] px-10 py-3 text-sm font-black tracking-wide opacity-0 transition-opacity duration-300 hover:bg-[#312691] group-hover:opacity-100"
+						onclick={() => (selectedItem = item)}>Quick View</button
+					>
 					{#if gridCols === 3}
 						<button
 							onclick={() => showPreviousImage(name)}
-							class="absolute bottom-[50%] rounded-full bg-orange opacity-50 hover:opacity-100 sm:right-[70%] sm:size-5 md:right-[85%] md:size-9"
+							class="absolute bottom-1/2 rounded-full bg-orange opacity-50 hover:opacity-100 sm:right-[70%] sm:size-5 md:right-[85%] md:size-9"
 							>&#11164;</button
 						>
 						<button
 							onclick={() => showNextImage(name)}
-							class="absolute bottom-[50%] rounded-full bg-orange opacity-50 hover:opacity-100 sm:left-[70%] sm:size-5 md:left-[85%] md:size-9"
+							class="absolute bottom-1/2 rounded-full bg-orange opacity-50 hover:opacity-100 sm:left-[70%] sm:size-5 md:left-[85%] md:size-9"
 							>&#11166;</button
 						>
 					{/if}
 				</div>
+
 				{#if gridCols === 3}
 					<div
 						class="relative flex justify-between bg-transparent md:items-center md:justify-center md:px-2"
@@ -208,7 +217,6 @@
 						</div>
 
 						<div class="px-2">
-							<!-- svelte-ignore a11y_consider_explicit_label -->
 							<button>
 								<Cart />
 							</button>
@@ -218,6 +226,85 @@
 			</div>
 		{/each}
 	</div>
+
+	{#if selectedItem}
+		<div
+			class="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-opacity-100"
+		>
+			<div
+				class="relative grid h-[40rem] w-[70rem] grid-cols-5 rounded-lg bg-gray-950 p-8 text-white hover:border-2 hover:border-white hover:shadow-2xl hover:shadow-white"
+			>
+				<button
+					class="absolute right-2 top-2 px-2 text-xl font-bold"
+					onclick={() => (selectedItem = null)}
+				>
+					&#10005;
+				</button>
+
+				<div class="mr-4 flex items-center justify-center">
+					<section class="flex size-20 flex-col items-center justify-center">
+						{#each selectedItem.image as image, index}
+							<button
+								type="button"
+								class="mb-2 flex cursor-pointer flex-col items-center rounded-md hover:opacity-80 focus:outline-none"
+								onclick={() => changeImage(index)}
+								onkeydown={(e) => e.key === 'Enter' && changeImage(index)}
+							>
+								<img
+									src={image}
+									alt={`Image ${index + 1}`}
+									class="h-[7rem] w-full rounded-md border border-white"
+								/>
+							</button>
+						{/each}
+					</section>
+				</div>
+
+				<!-- Main image section -->
+				<div class="col-span-2 flex grow flex-col items-center justify-center">
+					<img
+						src={selectedItem.image[currentIndexes[selectedItem.name]]}
+						alt="Selected Item"
+						class="mb-4 h-[30rem] w-9/12 rounded-md"
+					/>
+					<button
+						class="mt-4 rounded-full border border-white bg-black px-6 py-3 text-sm text-white active:border-2"
+						>View Product Details</button
+					>
+				</div>
+
+				<!-- Item details section -->
+				<div class="col-span-2 mt-5 flex flex-col justify-start px-6">
+					<h2 class=" mb-4 text-start text-3xl font-bold tracking-wider">
+						{selectedItem.description}
+					</h2>
+					<p class="mb-4 text-start text-2xl font-bold">${selectedItem.price}</p>
+					<h6 class="mb-8 text-start sm:text-[7px] md:text-sm">
+						<span
+							class={`${selectedItem.availability === 'Available' ? 'text-green-400' : 'text-red-400'} sm:text-[15px] md:text-[80px]`}
+						>
+							.
+						</span>
+						{selectedItem.availability}
+					</h6>
+					<button
+						class="rounded-full bg-blue-500 px-10 py-4 text-white opacity-100 active:opacity-70"
+						>Add to Cart</button
+					>
+					<section class="flex text-xs gap-6 mt-5">
+						<button class="hover:underline">Return Policy</button>
+						<button class="hover:underline">Shipping Policy</button>
+					</section>
+
+					<section class="flex flex-col text-xs  text-start mt-8 gap-2">
+						<p><span>&#9888</span> Product Warning/Cautions</p>
+						<p> <span>&#9888</span>  <strong>WARNING:</strong>  CHOKING HAZARD - Small Parts. Not for children under 5 years.</p>
+					</section>
+					<h2 class="font-bold absolute bottom-0 right-0 m-5">Mage Family <span>&#9827;</span></h2>
+				</div>
+			</div>
+		</div>
+	{/if}
 
 	<div class="my-3 flex content-center justify-center text-center md:h-[10rem]">
 		<button onclick={previousPage} disabled={currentPage === 0} class="hover:font-black"
